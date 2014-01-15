85b13819a0d2a5101418573bc57dd3edb8880fb7
Shaun McCormick <splittingred@gmail.com>
2014-01-14 22:57:49 -0600

    Add in devise views and name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |e|
     redirect_to root_url, alert: e.message
   end
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+          |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+          |u| u.permit(registration_params)
+      }
+    end
+  end
+
 end
