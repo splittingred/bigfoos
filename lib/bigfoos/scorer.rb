ba4eaf9d01a333af5863c1ae94c233883267d326
Shaun McCormick <splittingred@gmail.com>
2014-01-31 23:16:39 -0600

    score rank adjustment to not favor so heavy people with lots of games

--- lib/bigfoos/scorer.rb
+++ lib/bigfoos/scorer.rb
@@ -17,7 +17,7 @@ class BigFoos::Scorer
       :ppg => 35.0,
       :ppg_against => 20.0,
 
-      :match_percentage_multiplier => 10,
+      :match_percentage_multiplier => 0.2,
       :inactive_penalty_multiplier => 25,
       :inactive_penalty_days => 5.00,
       :minimum_matches => 5
@@ -35,6 +35,7 @@ class BigFoos::Scorer
       @score += self.win_ratio_adjustment
       #@score += self.inactivity_adjustment
       @score += self.ppg_adjustments
+      @score += self.percentage_of_games_adjustment
     end
     @score = @score <= 0 ? 0 : @score
     @user.score = @score
@@ -50,18 +51,22 @@ class BigFoos::Scorer
     adjustment = 0
     wins = @stats[:wins].to_i
     losses = @stats[:losses].to_i
-    player_games = @stats[:games].to_i
-    total_games = ::Game.where('status = ?','finished').count
-
-    percentage_of_games = (player_games.to_f / total_games.to_f)
-
     if wins > 0 or losses > 0
-      adjustment += (@user.wl_ratio * 10 * @ratios[:win_loss_ratio_multiplier])+(percentage_of_games*100*@ratios[:match_percentage_multiplier])
-        # taking this out as its skewing toward most games played
-        #*(percentage_of_games*100*@ratios[:match_percentage_multiplier]))
+      adjustment += @user.wl_ratio * 10 * @ratios[:win_loss_ratio_multiplier]
     end
 
     adjustment
+  end
+
+  ##
+  # Calculates score boost for games played
+  #
+  def percentage_of_games_adjustment
+    player_games = @stats[:games].to_i
+    total_games = ::Game.where('status = ?','finished').count
+    percentage_of_games = (player_games.to_f / total_games.to_f)
+
+    percentage_of_games * 10 * @ratios[:match_percentage_multiplier]
   end
 
   ##
