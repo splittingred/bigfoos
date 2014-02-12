c335d332ee57ee60e7ef2afb1b8955810302dd09
Bill Thompson <bill.thompson@bigcommerce.com>
2014-02-11 20:51:12 -0600

    seed test players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# :name, :email, :password, :password_confirmation, :provider, :uid
+
+# Create some test players
+(1..6).each do |i|
+  u = User.new(
+    :name => "Player - #{i}",
+    :email => "player+#{i}@example.com",
+    :password => '1234',
+    :password_confirmation => '1234',
+  )
+
+  u.save!(:validate => false)
+end
+
